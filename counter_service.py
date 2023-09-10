11df4565afc84dc3fb5e441477b27ced6de1e5f1
muzaparoff <muzyukins@gmail.com>
2023-09-10 10:44:37 +0300

    [FIX]workflow action seperate build and deploy and health check logic

--- counter_service.py
+++ counter_service.py
@@ -17,6 +17,13 @@ def post():
     counter += 1
     return "OK"
 
+@app.route("/health", methods=["GET"])
+def health_check():
+    # Add health check logic here
+    # You can perform any checks you need and return an appropriate response
+    # For simplicity, we'll just return a basic "OK" response.
+    return "OK"
+
 if __name__ == "__main__":
     http_server = WSGIServer(('0.0.0.0', 80), app)
     http_server.serve_forever()
