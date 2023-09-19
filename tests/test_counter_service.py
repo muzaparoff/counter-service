5ae5cfa581b3b187e8052321f6b43d0a1d230cc6
muzaparoff <muzyukins@gmail.com>
2023-09-19 12:00:22 +0300

    [Add]unittest support for async and rabbitmq

--- tests/test_counter_service.py
+++ tests/test_counter_service.py
@@ -1,20 +1,58 @@
 import unittest
+import asyncio
+from unittest.mock import Mock, patch
 from counter_service import app
+import pika
 
-class TestApp(unittest.TestCase):
+class TestCounterService(unittest.IsolatedAsyncioTestCase):
 
-    def setUp(self):
+    async def asyncSetUp(self):
+        # Initialize Flask app for testing
         self.app = app.test_client()
 
-    def test_index(self):
-        response = self.app.get('/')
-        self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.data.decode('utf-8'), '1')
+        # Create a mock RabbitMQ connection and channel for testing
+        self.mock_connection = Mock()
+        self.mock_channel = Mock()
 
-    def test_post(self):
-        response = self.app.post('/post')
+        async def mock_connection():
+            return self.mock_connection
+
+        async def mock_channel():
+            return self.mock_channel
+
+        # Patch pika's asynchronous methods
+        with patch('pika.AsyncioConnection', mock_connection), \
+             patch('pika.AsyncioChannel', mock_channel):
+            await self.asyncSetUpRabbitMQ()
+
+    async def asyncSetUpRabbitMQ(self):
+        # Your RabbitMQ setup code here (if any)
+
+    async def asyncTearDown(self):
+        # Clean up resources and connections
+        await self.app.close()
+        await self.mock_connection.close()
+
+    async def test_post_request_increments_counter(self):
+        # Simulate a POST request to increment the counter
+        response = await self.app.post('/')
         self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.data.decode('utf-8'), 'OK')
+
+        # Simulate a GET request to retrieve the counter value
+        response = await self.app.get('/')
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(b'Counter: 1', response.data)
+
+    async def test_multiple_post_requests(self):
+        # Simulate multiple POST requests to increment the counter
+        for _ in range(5):
+            response = await self.app.post('/')
+            self.assertEqual(response.status_code, 200)
+
+        # Simulate a GET request to retrieve the counter value
+        response = await self.app.get('/')
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(b'Counter: 5', response.data)
 
 if __name__ == '__main__':
     unittest.main()
